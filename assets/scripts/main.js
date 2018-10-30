83d9ba5077991bb5522ac3587e3688cd47124abc
Jordan Guy <jordan.guy.277@gmail.com>
2018-10-30 19:36:36 -0400

    menu slide with darkened background complete

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".sidebar");
+const background = document.querySelector('.backgroundActive');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle("is-active");
     nav.classList.toggle("active");
+    background.style.display = background.style.display === 'block' ? '' : 'block';
 });
