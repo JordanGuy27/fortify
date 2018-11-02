aa7020b7aafa14c3c3422e331fbcb2feb242c3aa
Jordan Guy <jordan.guy.277@gmail.com>
2018-11-02 19:57:58 -0400

    added smoothscroll and hover.css

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".sidebar");
-const background = document.querySelector('.backgroundActive');
+const background = document.querySelector(".backgroundActive");
 
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle("is-active");
-    nav.classList.toggle("active");
-    background.style.display = background.style.display === 'block' ? '' : 'block';
+$(document).ready(function () {
+  $('a').smoothScroll({
+    speed: 1300,
+    offset: -90,
+  });
+}); 
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle("is-active");
+  nav.classList.toggle("active");
+  background.style.display = background.style.display === 'block' ? '' : 'block';
+});
+nav.addEventListener('click', function () {
+  nav.classList.toggle("active");
+  hamburger.classList.toggle("is-active");
+  background.style.display = background.style.display === "block" ? "" : "block";
 });
